23e95c6b0c06f564b63277ee91a102b593e8d1ff
SudipGhara19 <sudipghara135@gmail.com>
2024-03-18 18:55:50 +0530

    validate the data of adding new product form before save the data

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -16,13 +16,35 @@ export default class ProductController{
 
     getAddForm(req, res) {
 
-        return res.render('new-product');
+        return res.render('new-product', {errorMessage: null});
     }
 
     addNewProduct(req, res){
+        //validate data
+        const { name, price, imageUrl } = req.body;
+        let errors = [];
+
+        if(!name || name.trim() == ''){
+            errors.push('Name is required!')
+        }
+
+        if(!price || parseFloat(price)<1){
+            errors.push('Price must be a positive Number.')
+        }
+
+        try{
+            const validURL = new URL(imageUrl);
+        }catch(err){
+            errors.push("URL is invalid.");
+        }
+
+        if(errors.length > 0){
+            return res.render('new-product', {errorMessage: errors[0]});
+        }
+
         console.log('New book added. Book Details: ' + req.body);
         ProductModel.add(req.body);
         const products = ProductModel.get();
         res.render('products', {products: products})
-    }
+    } 
 }
